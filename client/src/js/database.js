94cf270b8e2241522337fb5625ff2a94ba9a1c50
Liquid Monks <info@liquidmonks.com>
2023-03-13 10:08:07 -0500

    client/src/js/database.js adds support for IndexedDB, a browser-based database, to enable the storage of data in the 'jate' database. The code uses the 'openDB' function from the 'idb' package to initialize the database with an 'upgrade' function that creates the 'jate' object store with an 'id' key and an auto-incrementing value. The code also defines a 'putDb' function that adds data to the 'jate' database by starting a transaction, getting the object store, and adding the 'content' to the database. The commit also includes an error message for the unfinished 'putDb' function.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,42 @@
-import { openDB } from 'idb';
+// Import the 'openDB' function from the 'idb' package
+import { openDB } from "idb";
 
+// Define an async function to initialize the database
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
+    // Define an 'upgrade' function to create the database if it doesn't exist
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Define an async function to add data to the database
+export const putDb = async (content) => {
+  console.error("putDb not implemented");
+
+  // Open the 'jate' database
+  const contactDb = await openDB("jate", 1);
+
+  // Start a transaction to read and write data
+  const tx = contactDb.transaction("jate", "readwrite");
+
+  // Get the object store for the 'jate' database
+  const store = tx.objectStore("jate");
+
+  // Add the 'content' to the database
+  const request = store.put({ id: 1, value: content });
+
+  // Wait for the request to complete and log the result
+  const result = await request;
+  console.log("🚀 - data saved tot he database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error("getDb not implemented");
 
 initdb();
